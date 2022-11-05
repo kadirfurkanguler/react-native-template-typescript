74611383bc5d6d4f8b20225843722f07ac166119
kadirfurkanguler <furkan.guler@pigasoft.com>
2022-11-05 14:27:57 +0300

    removed logs

--- template/index.ts
+++ template/index.ts
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
-
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent(appName, () => App);
